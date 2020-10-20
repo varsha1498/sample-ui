72aafad784869e93b3cd08f259797631e3b381c1
Dey <sdey3002@7-11.com>
2020-10-20 12:53:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SearchIcon from '@material-ui/icons/Search';
+
+import Search from "./Components/Search";
+import Appbar from "./Components/Appbar"
+import Table from "./Components/Table"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Appbar/>
+     <div>
+     <SearchIcon style={{width:"80px",marginTop:"70px"}} />
+    
+     <text>Search</text>
+     
+       <button style={{marginLeft:"50px", height:"40px",backgroundColor:"green"}}> Create New product</button>
+     </div>
+     <Table/>
     </div>
   );
 }
